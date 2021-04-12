a7735c70cf5c6c75f414f87692f8d745d4f0a5ae
Noel-N-UMIM <72234343+Noel-N-UMIM@users.noreply.github.com>
2021-04-12 10:24:26 -0400

    Solved NaN display
    Woo hoo!  I solved (or worked around )the display of "NaN" when the user initiates the input fields.

--- Experiment/JS/Calculator.js
+++ Experiment/JS/Calculator.js
@@ -2,18 +2,25 @@ function CalcDayRate(){
 
     // Find the values from the input fields// 
     
-    let salary = document.getElementById('salary').value;
-    let expenses = document.getElementById('expenses').value;
+    let salary = parseFloat(0)+document.getElementById('salary').value;
+    let expenses = parseFloat(0)+document.getElementById('expenses').value;
+    
     
     let a = parseFloat(salary)+parseFloat(expenses);
     let dayRate=(a/260).toFixed(2);
     dayRate = dayRate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     //this adds a comma for the thousands, between every 3 digits in the string//
     document.getElementById('day_Rate').innerHTML = "= $"+""+ dayRate;
-    let days = document.getElementById('days').value;
+    let days = parseFloat(0)+document.getElementById('days').value;
+   
+//    -------Pricing the Job-------
     let jobPrice=(days*dayRate).toFixed(2);
     jobPrice = jobPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById('job_Price').innerHTML= "= $"+""+ jobPrice;
+
+    // if (document.getElementById('day_Rate').innerHTML == NaN){
+    //     document.getElementById('day_Rate').innerHTML=0;
+    // }
 }
 
 function CalcJobPrice(){
